e2a9f329c0ae9189fe9238c6e4d3af17d69bd8bc
alexuus <alexexmypride@gmail.com>
2026-04-01 17:40:52 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -73,6 +73,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 WSGI_APPLICATION = 'where_to_go.wsgi.application'
 
 
