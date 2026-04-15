f08a36e087a20b6f036550d10e8ec99a9833c1fb
alexuus <alexexmypride@gmail.com>
2026-04-15 17:36:16 +0300

    added preview

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,49 @@
 from django.contrib import admin
-from places import models
+from django.http import HttpRequest
+from places.models import Place, PlaceImage
+from django.utils.html import format_html
+from adminsortable2.admin import SortableTabularInline, SortableAdminBase
 
 
-class PlaceImageInline(admin.TabularInline):
-    model = models.PlaceImage
-    extra = 1
+def preview_inline(obj):
+    try:
+        return format_html(
+            '<img src="{}" style="max-width: 300px; max-height: 200px" />',
+            obj.image.url
+            )
+    except Exception as e:
+        return format_html(
+            '<span style="color: red;"> Ошибка загрузки: {}</span>',
+            e
+            )
 
 
-@admin.register(models.Place)
-class PlaceAdmin(admin.ModelAdmin):
-    inlines = [PlaceImageInline]
+class AdminInLine(SortableTabularInline):
+    model = PlaceImage
+    can_delete = False
+    extra = 0
+    fields = (('image', preview_inline))
+    readonly_fields = [preview_inline,]
+    verbose_name_plural = 'Фотографии'
+
+
+@admin.register(Place)
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
+    list_display = ['title',]
+    inlines = [AdminInLine]
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    def preview(obj):
+        return format_html(
+            '<img src="{}" style="max-width: 300px; max-height: 200px" />',
+            obj.image.url
+            )
+
+    list_display = ['place',]
+    raw_id_fields = ['place',]
+    readonly_fields = [preview,]
