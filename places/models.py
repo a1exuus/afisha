6ffe4a4b5597560bad8ec978fe4b83448dcf63a6
alexuus <alexexmypride@gmail.com>
2026-04-23 17:26:20 +0300

    got rid of `max_length` in a `short_description`

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Заголовок', max_length=100)
-    short_description = models.TextField('Краткое описание', max_length=800, default='Краткое описание', blank=True)
+    short_description = models.TextField('Краткое описание', default='Краткое описание', blank=True)
     long_description = HTMLField(verbose_name='Полное описание', blank=True)
     latitude = models.DecimalField('Широта', max_length=20, max_digits=20, decimal_places=16)
     longitude = models.DecimalField('Долгота', max_length=20, max_digits=20, decimal_places=16)
